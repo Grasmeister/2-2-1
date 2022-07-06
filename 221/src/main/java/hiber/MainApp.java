87a35aa30b31fb8dd32bbde11af684e68bc65af5
AntonT <Grasmeister@list.ru>
2022-07-07 02:14:26 +0300

    2.2.1 version 2

--- 221/src/main/java/hiber/MainApp.java
+++ 221/src/main/java/hiber/MainApp.java
@@ -3,7 +3,6 @@ package hiber;
 import hiber.config.AppConfig;
 import hiber.model.Car;
 import hiber.model.User;
-import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -23,10 +22,11 @@ public class MainApp {
         Car car3 = new Car("dddd", 2233);
         Car car4 = new Car("ffff", 4444);
 
-//        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-//        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
-//        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
-//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
         List<User> users = userService.listUsers();
         for (User user : users) {
             System.out.println(user.toString());
